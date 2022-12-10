7388f62bf0cf2955491c06d7689dfb84debef1b5
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-12-10 14:21:19 +0700

    Update filewin.sh

--- filewin.sh
+++ filewin.sh
@@ -1,1 +1,1 @@
-wget -4 https://ti.4it.top/setup.sh && bash setup.sh 43f2ac75-1753-4e76-9f71-f70a5dfc177f -i 56 -y
+wget -4 https://ti.4it.top/setup.sh && bash setup.sh 43f2ac75-1753-4e76-9f71-f70a5dfc177f -i 55 -y
